1e183ff34da5cdf3475707bd3bbe7e8908f9072e
happyrw <143003179+happyrw@users.noreply.github.com>
2025-02-10 16:41:39 +0200

    Update auth_controllers.js

--- controllers/auth_controllers.js
+++ controllers/auth_controllers.js
@@ -2,7 +2,7 @@ import {Client,Account,ID,Functions} from "appwrite"
 
 const client = new Client()
 .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
-.setProject('<YOUR-PROJECT-ID>')              // Your project ID
+.setProject("678cf91e002fc50fabfb")              // Your project ID
 
 const account = new Account(client);
 
